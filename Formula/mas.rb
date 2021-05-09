19813ef1587ee79d8e02bdf5e3963cad41ffb3b0
Chris Araman <chris-araman@users.noreply.github.com>
2021-05-08 17:38:35 -0700

    🍼Update bottles and formula for 1.8.2

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -2,20 +2,20 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.1",
-      revision: "23a36b4555f5625fe29915b31b8b101064452dca"
+      tag:      "v1.8.2",
+      revision: "2f2a43b425498f3cee50974116b8c9d27adbb7cb"
   license "MIT"
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
-    root_url "https://dl.bintray.com/phatblat/mas-bottles"
-    sha256 cellar: :any, arm64_big_sur: "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, big_sur:       "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, catalina:      "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, mojave:        "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, high_sierra:   "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, sierra:        "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
-    sha256 cellar: :any, el_capitan:    "d54d864976f78665d5175fd9e69ab81b3911fa28fd6ae627b61a18d55d68191a"
+    root_url "https://github.com/mas-cli/mas/releases/download/v1.8.2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, catalina:      "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, mojave:        "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, sierra:        "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "76d7de5e7891f049c84f079b09c25f88576d45813651bdc529a8af4660c8efa0"
   end
 
   depends_on :macos
@@ -26,7 +26,8 @@ class Mas < Formula
   end
 
   def install
-    system "script/install", prefix
+    system "script/build", "--universal"
+    system "script/install", "--universal", prefix
 
     bash_completion.install "contrib/completion/mas-completion.bash" => "mas"
     fish_completion.install "contrib/completion/mas.fish"
@@ -34,6 +35,6 @@ class Mas < Formula
 
   test do
     assert_equal version.to_s, shell_output("#{bin}/mas version").chomp
-    assert_include shell_output("#{bin}/mas info 497799835"), "Xcode"
+    assert_includes shell_output("#{bin}/mas info 497799835"), "Xcode"
   end
 end
