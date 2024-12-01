70c44d1ceccee73f58e9ffd7c11890402778435c
Anka <runner@Mac-1733077583704.local>
2024-12-01 19:34:38 +0000

    mas 1.8.8-beta.13
    Closes #60.
    
    Signed-off-by: Ben Chatelain <28851+phatblat@users.noreply.github.com>

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -6,8 +6,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.7",
-      revision: "4405807010987802c0967bbf349c08808062b824"
+      tag:      "v1.8.8-beta.13",
+      revision: "5a6314fe36056bd077ee0b1c08924b169083b9d1"
   license "MIT"
   head "https://github.com/mas-cli/mas.git", branch: "main"
 
