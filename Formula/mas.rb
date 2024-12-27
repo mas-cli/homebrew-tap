48fed3e0417f7b7114c8a7281899f47f98cc3e02
Anka <runner@Mac-1735275751066.local>
2024-12-27 06:14:57 +0000

    mas 1.8.8
    Closes #90.
    
    Signed-off-by: workflow-service-account[bot] <192765064+workflow-service-account[bot]@users.noreply.github.com>

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -6,8 +6,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      tag:      "v1.8.8-beta.13",
-      revision: "5a6314fe36056bd077ee0b1c08924b169083b9d1"
+      tag:      "v1.8.8",
+      revision: "26964a86206241f95be175a2be26218e8fc017a9"
   license "MIT"
   head "https://github.com/mas-cli/mas.git", branch: "main"
 
